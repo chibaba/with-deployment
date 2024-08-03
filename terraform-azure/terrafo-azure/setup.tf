5471b6c55fc104736fc7a00644f23e05c825fcc3
chibaba <chinedu.onyeoka@gmail.com>
2024-08-03 18:33:42 +0100

    setting up

--- terraform-azure/terrafo-azure/setup.tf
+++ terraform-azure/terrafo-azure/setup.tf
@@ -13,5 +13,26 @@ terraform {
     azurecaf = {
         source = "aztfmod/azurecaf" # https://registry.terraform.io/providers/aztfmod/azurecaf/latest
     }
+
+    random = {
+        source = "hasicorp/random" # https://registry.terraform.io/providers/hasicorp/latest
+    }
+
+    http = {
+        source = "hasicorp/http" # https://registry.terraform.io/providers/hasicorp/latest
+    }
   }
+}
+
+# configure the microsoft azure provider
+provider "azurerm" {
+    features {
+    }
+
+}
+  # Use the the Azure Region module to get the short name of the Azure region for more
+# details see: https://registry.terraform.io/modules/claranet/regions/azurerm/latest 
+   module "azure_region" {
+   source       = "claranet/regions/azurerm"
+   azure_region = var.location
 }
