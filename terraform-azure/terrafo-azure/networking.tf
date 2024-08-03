c51eb1374d391097142cae433baba40ef74bdfa1
chibaba <chinedu.onyeoka@gmail.com>
2024-08-03 19:55:02 +0100

    netwrkn

--- terraform-azure/terrafo-azure/networking.tf
+++ terraform-azure/terrafo-azure/networking.tf
@@ -20,3 +20,29 @@ resource "azurecaf_name" "load_balancer" {
     suffixes = [var.environment_type, module.azure_region.location_short]
     clean_input = true  
 }
+
+resource "azurecaf_name" "load_balancer_pip" { 
+    name = azurecaf_name.load_balancer.result
+    resource_type = "azurerm_public_ip"
+    clean_input = true
+}
+
+resource "azurecaf_name" "nsg" {
+    name = var.name
+    resource_type = "azurerm_network_security_group"
+    suffixes = [var.environment_type, module.azure_region.location_short]
+    clean_input = true
+}
+# This block of code creates a resource of type "azurerm_virtual_network" and assigns it to the variable "vnet"
+resource "azurerm_virtual_network" "vnet" {
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  name = azurecaf_name.vnet.result
+  address_space = var.vnet_address_space
+  tags = var.default_tags
+}
+
+# Create an azurerm_subnet resource for each subnet in the virtual network using the vnet_subnets variable
+resource "" "name" {
+  
+}
